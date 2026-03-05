756425fefeafe168feeac2e77ef2fbe2e71f0733
himesh-mehta <himesh.mehta@somaiya.edu>
2026-03-05 11:21:24 +0530

    upated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Chatbot from '@/components/Chatbot';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +30,6 @@ export default function RootLayout({
       >
         <TooltipProvider>
           {children}
-          <Chatbot />
         </TooltipProvider>
       </body>
     </html>
