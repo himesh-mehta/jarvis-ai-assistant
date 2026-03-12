7d7f63e3974da86839b8e8556cb857cbf875d134
A-man06 <amayadav06@gmail.com>
2026-03-12 14:43:47 +0530

    chore: remove untracked files from git index

--- Jarvis/test_dns_lookup.js
+++
@@ -1,9 +0,0 @@
-const dns = require('dns');
-
-dns.lookup('jarvis.upmkip9.mongodb.net', (err, addresses) => {
-  if (err) {
-    console.error('DNS lookup failed:', err);
-    return;
-  }
-  console.log('Lookup Addresses:', addresses);
-});
