7a672896ff93b2c53c42b55fc44c1ef449f7afd8
himesh-mehta <himesh.mehta@somaiya.edu>
2026-03-14 15:07:03 +0530

    security: remove hardcoded MongoDB URI from test script

--- Jarvis/scripts/test_mongodb_standard.js
+++ Jarvis/scripts/test_mongodb_standard.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI = "mongodb://himeshmehta_db_user:Himesh123@ac-k5pzrez-shard-00-00.upmkip9.mongodb.net:27017,ac-k5pzrez-shard-00-01.upmkip9.mongodb.net:27017,ac-k5pzrez-shard-00-02.upmkip9.mongodb.net:27017/jarvis_db?ssl=true&authSource=admin&retryWrites=true&w=majority";
+const MONGODB_URI = process.env.MONGODB_URI || "your_mongodb_uri_here";
 
 mongoose.connect(MONGODB_URI)
   .then(() => {
